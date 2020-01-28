74c11994e6cecdf9b4c6820e405b32d6f6a6812f
sandeep kousik <sandeepc@linkstreet.in>
2020-01-28 11:44:50 +0530

    stage scripts

---
+++ stage/stage-adapt.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd /var/www/webroots/stage-adapt.linkstreet.co
+
+
+
+git fetch --all && git checkout composer.lock && git stash && git checkout $site_version && git stash pop
+
+sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ bootstrap/
+
+composer update ultron/adapt-portal
+
+sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ bootstrap/
+
+php artisan seed --class=ProgramCertificatesTableSeeder 
+
+php artisan config:clear 
+
+cp /opt/phpcredentials/adapt-credentials.php /tmp/credentials.php && cp /opt/ultron-core-scripts/php7/2019_12_25_update_adapt_certificate_template_design.php /tmp/
+
+php /tmp/2019_12_25_update_adapt_certificate_template_design.php
+
+php artisan portal:publish && php artisan cache:clear
+
+cp .env .env-bkf
+
+sed '6 s/.*/APP_SITE_VERSION="'$site_version'"/' .env
+
+echo $'\n#JWPLAYER Fallback\nFALLBACK_JW_PLAYER=false' >> .env
+
+cd /tmp/ && rm credentials.php
--- stage/stage-shell.sh
+++ stage/stage-shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/www/webroots/test-adapt
+cd /var/www/webroots/stage-shell.linkstreet.co
 
 
 
@@ -8,7 +8,7 @@ git fetch --all && git checkout composer.lock && git stash && git checkout $site
 
 sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ bootstrap/
 
-composer update ultron/adapt-portal
+composer update ultron/shell-portal
 
 sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ bootstrap/
 
@@ -16,12 +16,17 @@ php artisan seed --class=ProgramCertificatesTableSeeder
 
 php artisan config:clear 
 
-cp /opt/phpcredentials/adapt-credentials.php /tmp/credentials.php && cp /opt/ultron-core-scripts/php7/2019_12_25_update_adapt_certificate_template_design.php /tmp/
+cp /opt/phpcredentials/shell-credentials.php /tmp/credentials.php && cp /opt/ultron-core-scripts/php7/2020_01_27_update_serial_number_in_user_certificates.php /tmp/
 
-php /tmp/2019_12_25_update_adapt_certificate_template_design.php
+php /tmp/2020_01_27_update_serial_number_in_user_certificates.php
 
 php artisan portal:publish && php artisan cache:clear
 
+cp .env .env-bkf
+
 sed '6 s/.*/APP_SITE_VERSION="'$site_version'"/' .env
 
+echo $'\n#JWPLAYER Fallback\nFALLBACK_JW_PLAYER=false' >> .env
+
+cd /tmp/ && rm credentials.php
 
