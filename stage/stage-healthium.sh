97e15642cf332a9010319cfbfd3f71c908d58118
sandeep kousik <sandeepc@linkstreet.in>
2020-01-29 17:43:35 +0530

    stage-healthium

--- stage/stage-healthium.sh
+++ stage/stage-healthium.sh
@@ -12,11 +12,17 @@ composer update ultron/healthium-portal
 
 sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ bootstrap/
 
+echo "Program certificate seeder ran"
 php artisan db:seed --class=ProgramCertificatesTableSeeder 
+echo ""
 
+echo "Application config has been cleared"
 php artisan config:clear 
+echo ""
 
+echo "php update serial certificate script executed successfully"
 cp /opt/phpcredentials/healthium-credentials.php /tmp/credentials.php && cp /opt/ultron-core-scripts/php7/2020_01_27_update_serial_number_in_user_certificates.php /tmp/
+echo ""
 
 php /tmp/2020_01_27_update_serial_number_in_user_certificates.php
 
@@ -28,5 +34,5 @@ sed '6 s/.*/APP_SITE_VERSION="'$site_version'"/' .env
 
 echo $'\n#JWPLAYER Fallback\nFALLBACK_JW_PLAYER=false' >> .env
 
-cd /tmp/ && rm credentials.php
+#cd /tmp/ && rm credentials.php
 
