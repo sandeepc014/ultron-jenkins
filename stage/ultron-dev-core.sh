b384e6736170b96f515d3079fbc0eff311b7eeac
sandeep kousik <sandeepc@linkstreet.in>
2020-03-17 22:24:06 +0530

    core changes

--- stage/ultron-dev-core.sh
+++ stage/ultron-dev-core.sh
@@ -3,7 +3,7 @@
 cd /var/www/ultron/ultron-dev.linkstreet.co
 
 echo ""
-echo "Pulling latest commit into ultron-core from Dev branch"
+echo "<<<</*****Pulling latest commit into ultron-core from Dev branch*****/>>"
 echo ""
 
 git pull origin dev
@@ -12,7 +12,9 @@ sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ 
 
 echo ""
 echo ""
-echo "Here the five latest commit of ultron-core from Dev branch"
+echo "<<****Here the five latest commit of ultron-core from Dev branch****>>"
+echo ""
+echo ""
 git log -n5
 echo ""
 
