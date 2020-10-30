f5635c8a461951c91f61008fa7ec8c5c4a96bc35
sandeepc014 <53991810+sandeepc014@users.noreply.github.com>
2020-10-30 15:47:00 +0530

    Update ultron-dev-portal.sh

--- stage/ultron-dev-portal.sh
+++ stage/ultron-dev-portal.sh
@@ -11,6 +11,20 @@ cd /var/www/mount/ultron/ultron-dev.linkstreet.co/vendor/ultron/default-portal/s
 
 git pull
 
+echo ""
+echo "The branch name is"
+echo ""
+echo ""
+
+echo ""
+echo "The latest five commits of ultron-lang branch"
+echo ""
+
+git log -n5
+
+echo ""
+echo ""
+
 php artisan portal:publish && php artisan cache:clear
 
 cd /var/www/mount/ultron/ultron-dev.linkstreet.co/vendor/ultron/default-portal
