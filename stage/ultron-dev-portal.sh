8195cffc04f9ed1883df398a86b4219fa816b209
sandeepc014 <53991810+sandeepc014@users.noreply.github.com>
2020-11-18 15:03:53 +0530

    Update ultron-dev-portal.sh

--- stage/ultron-dev-portal.sh
+++ stage/ultron-dev-portal.sh
@@ -11,31 +11,13 @@ cd /var/www/mount/ultron/ultron-dev.linkstreet.co/vendor/ultron/default-portal/s
 
 git pull origin master
 
-echo ""
-echo "The branch name is"
-echo ""
-echo ""
-
 cd /var/www/mount/ultron/ultron-dev.linkstreet.co
 
 sudo chown -R ubuntu:www-data storage bootstrap && sudo chmod -R 775 storage bootstrap
 
 php artisan view:clear && php artisan config:clear && php artisan route:clear && php artisan portal:publish && php artisan cache:clear
 
-git branch
-
-echo ""
-echo "The latest five commits of ultron-lang branch"
-echo ""
-
-git log -n5
-
-echo ""
-echo ""
-
-
 cd /var/www/mount/ultron/ultron-dev.linkstreet.co/vendor/ultron/default-portal
-
 
 echo ""
 echo "The branch name is"
