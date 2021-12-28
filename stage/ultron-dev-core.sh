16717a8d24d7c268e4d55193f9d0e2b2b3fe46ab
Sandeep Kousik <53991810+sandeepc014@users.noreply.github.com>
2021-12-28 09:56:23 +0530

    Update ultron-dev-core.sh

--- stage/ultron-dev-core.sh
+++ stage/ultron-dev-core.sh
@@ -3,16 +3,16 @@
 cd /var/www/mount/ultron/ultron-dev.linkstreet.co
 
 echo ""
-echo "<<<</*****Pulling latest commit into ultron-core from  bug-fix/zoom-registration-issue branch*****/>>"
+echo "<<<</*****Pulling latest commit into ultron-core from  dev*****/>>"
 echo ""
 
-git pull origin bug-fix/zoom-registration-issue
+git pull origin dev
 
 sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ bootstrap/
 
 echo ""
 echo ""
-echo "<<****Here the five latest commit of ultron-core from  bug-fix/zoom-registration-issue branch****>>"
+echo "<<****Here the five latest commit of ultron-core from  dev****>>"
 echo ""
 echo ""
 git log -n5
