9cf25e881d0cc6438e7f264a32befcf43e225a45
Sandeep Kousik <53991810+sandeepc014@users.noreply.github.com>
2022-01-03 09:35:13 +0530

    Update ultron-dev-portal.sh

--- stage/ultron-dev-portal.sh
+++ stage/ultron-dev-portal.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /var/www/mount/ultron/ultron-dev.linkstreet.co
+cd /var/www/mvol/ultron/ultron-dev.linkstreet.co
 
 
 sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ bootstrap/
 
 composer update ultron/default-portal
 
-cd /var/www/mount/ultron/ultron-dev.linkstreet.co/vendor/ultron/default-portal/src/Portal/resources/lang
+cd /var/www/mvol/ultron/ultron-dev.linkstreet.co/vendor/ultron/default-portal/src/Portal/resources/lang
 
 git pull origin master
 
@@ -17,7 +17,7 @@ sudo chown -R ubuntu:www-data storage bootstrap && sudo chmod -R 775 storage boo
 
 sudo php artisan view:clear && sudo php artisan config:clear && sudo php artisan route:clear && sudo php artisan portal:publish && sudo php artisan cache:clear
 
-cd /var/www/mount/ultron/ultron-dev.linkstreet.co/vendor/ultron/default-portal
+cd /var/www/mvol/ultron/ultron-dev.linkstreet.co/vendor/ultron/default-portal
 
 echo ""
 echo "The branch name is"
